7909d23585fe9ec27bc88812afbc9e723bbea192
TestName <24ci0211@jec.ac.jp>
2025-08-27 20:12:11 +0900

    wii

---
+++ Source/BreakCityByDragon/Private/UI/DamageText.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/DamageText.h"
+
+int32 UDamageText::NativePaint(const FPaintArgs& Args, const FGeometry& AllottedGeometry, const FSlateRect& MyCullingRect, FSlateWindowElementList& OutDrawElements, int32 LayerId,
+	const FWidgetStyle& InWidgetStyle, bool bParentEnabled) const
+{
+	// ‹éŒ`•`‰æ
+	LayerId++;
+	FVector2D Position = AllottedGeometry.GetLocalSize() * 0.5f;
+	FVector2D Size = Brush.GetImageSize();
+	FSlateDrawElement::MakeBox(OutDrawElements, LayerId, AllottedGeometry.ToPaintGeometry(Position, Size), &Brush);
+
+	return FMath::Max(LayerId, LayerId);
+}
