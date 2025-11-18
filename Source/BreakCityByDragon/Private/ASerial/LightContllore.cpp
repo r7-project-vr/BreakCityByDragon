b0aea4e87ae6a64b77a15a46834732b5884a2053
Bisu1noshin <24ci0211@jec.ac.jp>
2025-11-18 18:19:46 +0900

    ライトの変換用のクラス 関数プールをつくって適切に通信を行う

---
+++ Source/BreakCityByDragon/Private/ASerial/LightContllore.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ASerial/LightContllore.h"
+
+LightContllore::LightContllore()
+{
+}
+
+LightContllore::~LightContllore()
+{
+}
---
+++ Source/BreakCityByDragon/Public/ASerial/LightContllore.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ASerialLibControllerWin.h"
+
+/**
+ * 
+ */
+class BREAKCITYBYDRAGON_API LightContllore
+{
+public:
+	LightContllore();
+	~LightContllore();
+};
